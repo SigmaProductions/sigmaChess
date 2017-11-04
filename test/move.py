82114851a1a38bf3f8a706c6c909d36327d2d64e
Ekierkad <daniel_1998k@interia.pl>
2017-11-04 21:46:08 +0100

    pawn attack fixed

--- test/move.py
+++ test/move.py
@@ -7,26 +7,8 @@ board= chessBoard()
 
 board.PRINTBOARD()
 
-print("\n")
-
-piece= board.getPiece(1,1)
-board.movePiece(piece,1,3)
-
-board.PRINTBOARD()
-
-
-print("\n")
-
-piece= board.getPiece(2,6)
-board.movePiece(piece,2,4)
-
-board.PRINTBOARD()
-
-
-print("\n")
-
-piece= board.getPiece(2,4)
-board.movePiece(piece,1,3)
+piece= board.getPiece(6,1)
+board.movePiece(piece,6,2)
 
 board.PRINTBOARD()
 
