f41d340e63c2e0ffda1d59b465b56d65a7ffb388
Bartosz Sendek <sendek.bartosz@gmail.com>
2017-11-05 07:24:03 +0100

    add basic functionality to networking; add connection test

--- controller/networking/multiplayer.py
+++ controller/networking/multiplayer.py
@@ -1,22 +1,40 @@
 import socket
 import threading
-
+from controller.networking.move import movePacket
+import pickle
 class ConnectionHandler:
 
     def __init__(self):
         self.port= 9332
-        self.address= "192.168.88.13"
+        self.address= "192.168.88.17"
 
-        self.receiver= socket.socket(family=socket.AF_INET)
-        self.receiver.bind((self.address,self.port))
+
+        self.receiver = socket.socket(family=socket.AF_INET)
+        self.transmitter= socket.socket(family=socket.AF_INET)
+
+        self.receiver.bind(('',self.port))
         self.receiver.listen()
 
-        self.receiverThread= threading.Thread(target=self.Echo)
+        self.receiverThread= threading.Thread(target=self.__Await)
         self.receiverThread.start()
 
-    def Echo(self):
-        connection, address = self.receiver.accept()
 
-        data= connection.recv(1024)
-        print(data)
-        connection.sendall(data)
+
+    def __Await(self):
+        connection, address = self.receiver.accept()
+        moveSerial= connection.recv(1024)
+
+        move= pickle.loads(moveSerial)
+        print(move)
+
+    def __SendData(self,data):
+        self.transmitter.connect((self.address,self.port))
+        self.transmitter.sendall(data)
+
+    def SendMove(self, pieceToMove, targetCoords):
+        packet= movePacket((pieceToMove.x,pieceToMove.y), targetCoords)
+        packetSerial= pickle.dumps(packet)
+
+        self.transmitterThread = threading.Thread(target=self.__SendData, args=[packetSerial])
+        self.transmitterThread.start()
+
