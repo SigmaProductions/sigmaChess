bd0da24416ed2f2e13d31d87d3a99ecb11345399
Altair97 <przemyslaw.reg@gmail.com>
2017-11-06 22:29:16 +0100

    fix boardtemplate path

--- board.py
+++ board.py
@@ -5,7 +5,6 @@ class chessBoard:
 
 
     def __init__(self):
-        self.Path = os.path.dirname(os.path.abspath(__file__))
         self.boardArray = [[]]
         self.boardArray = self.__spawnPieces()
         self.whoMoved = factionColor.FACTION_BLACK
@@ -19,8 +18,9 @@ class chessBoard:
         return hArray
 
     def __spawnPieces(self):
+        Path = os.path.dirname(os.path.abspath(__file__))
         boardArray = self.__createEmptyArray()
-        boardtemplate = open( self.Path+ "\\boardtemplate.txt", "r" )
+        boardtemplate = open(Path+ "\\boardtemplate.txt", "r")
         for y in range(8):
             line = boardtemplate.readline()
             for x in range(8):
